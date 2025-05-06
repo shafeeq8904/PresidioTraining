fdcd2a8c798a3fff9520df6d10070cce63aec7d1
shafeeq8904 <shafeeq8904@gmail.com>
2025-05-06 17:35:12 +0530

    task

---
+++ Day-2/integrityConstraints.sql
@@ -0,0 +1,103 @@
+CREATE TABLE DEPARTMENT (
+    deptname VARCHAR(100) PRIMARY KEY,
+    floor INT,
+    phone VARCHAR(20) NOT NULL UNIQUE,
+    empno INT 
+);
+-- Here i have removed the fk from the department table to the employee table, added later after creating the employee table.
+
+
+CREATE TABLE EMP (
+    empno INT PRIMARY KEY,
+    empname VARCHAR(100),
+    salary DECIMAL(10, 2),
+    deptname VARCHAR(100) NOT NULL,
+    bossno INT,
+    FOREIGN KEY (deptname) REFERENCES DEPARTMENT(deptname),
+    FOREIGN KEY (bossno) REFERENCES EMP(empno)
+);
+
+INSERT INTO DEPARTMENT (deptname, floor, phone, empno) VALUES
+('Management', 5, '34', 1),
+('Books', 1, '81', 4),
+('Clothes', 2, '24', 4),
+('Equipment', 3, '57', 3),
+('Furniture', 4, '14', 3),
+('Navigation', 1, '41', 3),
+('Recreation', 2, '29', 4),
+('Accounting', 5, '35', 5),
+('Purchasing', 5, '36', 7),
+('Personnel', 5, '37', 9),
+('Marketing', 5, '38', 2);
+
+INSERT INTO EMP (empno, empname, salary, deptname, bossno) VALUES
+(1, 'Alice', 75000, 'Management', NULL),
+(2, 'Ned', 45000, 'Marketing', 1),
+(3, 'Andrew', 25000, 'Marketing', 2),
+(4, 'Clare', 22000, 'Marketing', 2),
+(5, 'Todd', 38000, 'Accounting', 1),
+(6, 'Nancy', 22000, 'Accounting', 5),
+(7, 'Brier', 43000, 'Purchasing', 1),
+(8, 'Sarah', 56000, 'Purchasing', 7),
+(9, 'Sophile', 35000, 'Personnel', 1),
+(10, 'Sanjay', 15000, 'Navigation', 3),
+(11, 'Rita', 15000, 'Books', 4),
+(12, 'Gigi', 16000, 'Clothes', 4),
+(13, 'Maggie', 11000, 'Clothes', 4),
+(14, 'Paul', 15000, 'Equipment', 3),
+(15, 'James', 15000, 'Equipment', 3),
+(16, 'Pat', 15000, 'Furniture', 3),
+(17, 'Mark', 15000, 'Recreation', 3);
+
+ALTER TABLE DEPARTMENT
+ADD CONSTRAINT FK_DEPT_EMP
+FOREIGN KEY (empno) REFERENCES EMP(empno);
+
+CREATE TABLE ITEM (
+    itemname VARCHAR(100) PRIMARY KEY,
+    itemtype VARCHAR(100),
+    itemcolor VARCHAR(100)
+);
+
+CREATE TABLE Task_SALES (
+    salesno INT PRIMARY KEY,
+    salesqty INT CHECK (salesqty > 0),
+    itemname VARCHAR(100) NOT NULL,
+    deptname VARCHAR(100) NOT NULL,
+    FOREIGN KEY (deptname) REFERENCES DEPARTMENT(deptname),
+    FOREIGN KEY (itemname) REFERENCES ITEM(itemname)
+);
+
+INSERT INTO ITEM (itemname, itemtype, itemcolor) VALUES
+('Pocket Knife-Nile', 'E', 'Brown'),
+('Pocket Knife-Avon', 'E', 'Brown'),
+('Compass', 'N', NULL),
+('Geo positioning system', 'N', NULL),
+('Elephant Polo stick', 'R', 'Bamboo'),
+('Camel Saddle', 'R', 'Brown'),
+('Sextant', 'N', NULL),
+('Map Measure', 'N', NULL),
+('Boots-snake proof', 'C', 'Green'),
+('Pith Helmet', 'C', 'Khaki'),
+('Hat-polar Explorer', 'C', 'White'),
+('Exploring in 10 Easy Lessons', 'B', NULL),
+('Hammock', 'F', 'Khaki'),
+('How to win Foreign Friends', 'B', NULL),
+('Map case', 'E', 'Brown'),
+('Safari Chair', 'F', 'Khaki'),
+('Safari cooking kit', 'F', 'Khaki'),
+('Stetson', 'C', 'Black'),
+('Tent - 2 person', 'F', 'Khaki'),
+('Tent -8 person', 'F', 'Khaki');
+
+INSERT INTO Task_SALES (salesno, salesqty, itemname, deptname) VALUES
+(101, 2, 'Boots-snake proof', 'Clothes'),
+(102, 1, 'Pith Helmet', 'Clothes'),
+(103, 1, 'Sextant', 'Navigation'),
+(104, 3, 'Hat-polar Explorer', 'Clothes'),
+(105, 5, 'Pith Helmet', 'Equipment'),
+(106, 2, 'Pocket Knife-Nile', 'Clothes'),
+(107, 3, 'Pocket Knife-Nile', 'Recreation'),
+(108, 1, 'Compass', 'Navigation'),
+(109, 2, 'Geo positioning system', 'Navigation'),
+(110, 5, 'Map Measure', 'Navigation');
