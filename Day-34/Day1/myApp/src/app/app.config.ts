6c480485736cf144d015339a6943f472c751898f
shafeeq8904 <shafeeq8904@gmail.com>
2025-06-24 10:24:10 +0530

    testing in angular

---
+++ Day-34/Day1/myApp/src/app/app.config.ts
@@ -0,0 +1,25 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+import { UserService } from './services/UserService';
+import { AuthGuard } from './auth-guard';
+import { NotificationService } from './services/notification.service';
+import { BulkInsertService } from './services/BulkInsertService';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    BulkInsertService,
+    ProductService,
+    UserService,
+    NotificationService,
+    AuthGuard
+  ]
+};
